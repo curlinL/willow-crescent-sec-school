a578baac459b4d19fdf6d4c464cce718193dbc6a
Curlin Louw <86859438+curlinL@users.noreply.github.com>
2025-11-25 15:54:35 +0200

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://willow-crescent-sec-school.onrender.com/api',
 });
 
 export default api;
